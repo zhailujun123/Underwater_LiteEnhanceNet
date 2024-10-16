ed95cc6ed3ad2e4effc0978b1bdb2268d7079e9e
zhailujun123 <zhailujun123@gmail.com>
2024-10-15 23:45:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,29 +8,21 @@ from model import *
 from dataloader import myDataSet
 from metrics_calculation import *
 import os
-
-import time
-import argparse
-import numpy as np
 from PIL import Image
 from glob import glob
 from ntpath import basename
 from os.path import join, exists
-# pytorch libs
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision.utils import save_image
-import torchvision.transforms as transforms
 import imageio
-import PIL.Image
-import scipy.misc
 from utils.uqim_utils import getUIQM
 from utils.ssm_psnr_utils import getSSIM, getPSNR
-import itertools
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 __all__ = [
     "test",
     "setup",
@@ -53,10 +45,9 @@ def setup(config):
         config.device = "cpu"
 
     model = torch.load(config.snapshot_path).to(config.device)
-    transform = transforms.Compose([transforms.Resize((config.resize,config.resize)),transforms.ToTensor()]) ## 这里修改图像尺寸
-    # transform = transforms.Compose([transforms.Resize((1295, 1800)), transforms.ToTensor()])
-    test_dataset = myDataSet(config.test_images_path,None,transform, False)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset,batch_size = config.batch_size,shuffle = False)
+    transform = transforms.Compose([transforms.Resize((config.resize, config.resize)), transforms.ToTensor()])
+    test_dataset = myDataSet(config.test_images_path, None, transform, False)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False)
     print("Test Dataset Reading Completed.")
     return test_dataloader, model
 
@@ -64,11 +55,10 @@ def testing(config):
     ds_test, model = setup(config)
     test(config, ds_test, model)
 
-
 def SSIMs_PSNRs(gtr_dir, gen_dir, im_res=(256, 256)):
     """
-        - gtr_dir contain ground-truths
-        - gen_dir contain generated images
+        - gtr_dir contains ground-truth images
+        - gen_dir contains generated images
     """
     gtr_paths = sorted(glob(join(gtr_dir, "*.*")))
     gen_paths = sorted(glob(join(gen_dir, "*.*")))
@@ -77,21 +67,46 @@ def SSIMs_PSNRs(gtr_dir, gen_dir, im_res=(256, 256)):
         gtr_f = basename(gtr_path).split('.')[0]
         gen_f = basename(gen_path).split('.')[0]
         if (gtr_f == gen_f):
-            # assumes same filenames
             r_im = Image.open(gtr_path).resize(im_res)
             g_im = Image.open(gen_path).resize(im_res)
-            # get ssim on RGB channels
+            # Get SSIM on RGB channels
             ssim = getSSIM(np.array(r_im), np.array(g_im))
             ssims.append(ssim)
-            # get psnt on L channel (SOTA norm)
-            r_im = r_im.convert("L");
+            # Get PSNR on L channel (SOTA norm)
+            r_im = r_im.convert("L")
             g_im = g_im.convert("L")
             psnr = getPSNR(np.array(r_im), np.array(g_im))
             psnrs.append(psnr)
     return np.array(ssims), np.array(psnrs)
 
+def SSIMs_PSNRs_raw(gtr_dir, raw_dir, im_res=(256, 256)):
+    """
+        - gtr_dir contains ground-truth images
+        - raw_dir contains raw input images
+    """
+    gtr_paths = sorted(glob(join(gtr_dir, "*.*")))
+    raw_paths = sorted(glob(join(raw_dir, "*.*")))
+    ssims, psnrs = [], []
+    for gtr_path, raw_path in zip(gtr_paths, raw_paths):
+        gtr_f = basename(gtr_path).split('.')[0]
+        raw_f = basename(raw_path).split('.')[0]
+        if (gtr_f == raw_f):
+            r_im = Image.open(gtr_path).resize(im_res)
+            raw_im = Image.open(raw_path).resize(im_res)
+            # Get SSIM on RGB channels
+            ssim = getSSIM(np.array(r_im), np.array(raw_im))
+            ssims.append(ssim)
+            # Get PSNR on L channel (SOTA norm)
+            r_im = r_im.convert("L")
+            raw_im = raw_im.convert("L")
+            psnr = getPSNR(np.array(r_im), np.array(raw_im))
+            psnrs.append(psnr)
+    return np.array(ssims), np.array(psnrs)
 
 def measure_UIQMs(dir_name, im_res=(256, 256)):
+    """
+    - dir_name contains images to calculate UIQM
+    """
     paths = sorted(glob(join(dir_name, "*.*")))
     uqims = []
     for img_path in paths:
@@ -103,13 +118,14 @@ def measure_UIQMs(dir_name, im_res=(256, 256)):
 if __name__ == '__main__':
     models = "./snapshots/model_epoch_" + str(99) + ".ckpt"
     parser = argparse.ArgumentParser()
-    parser.add_argument('--snapshot_path', type=str, default=models, help='one')
-    parser.add_argument('--test_images_path', type=str, default="./data/raw/", help='path of input images(underwater images) for testing default:./data/input/')
-    parser.add_argument('--output_images_path', type=str, default='./data/output/', help='path to save generated image.')
-    parser.add_argument('--batch_size', type=int, default=1, help="default : 1")
-    parser.add_argument('--resize', type=int, default=256, help="resize images, default:resize images to 256*256")
-    parser.add_argument('--calculate_metrics', type=bool, default=False, help="calculate PSNR, SSIM and UIQM on test images")
-    parser.add_argument('--label_images_path', type=str, default="./data/label/", help='path of label images(clear images) default:./data/label/')
+    parser.add_argument('--snapshot_path', type=str, default=models, help='Model snapshot path')
+    parser.add_argument('--test_images_path', type=str, default="./data/raw/", help='Path of input images (underwater images) for testing')
+    parser.add_argument('--output_images_path', type=str, default='./data/output/', help='Path to save generated images.')
+    parser.add_argument('--raw_images_path', type=str, default='./data/raw/', help='Path to raw input images for comparison')
+    parser.add_argument('--batch_size', type=int, default=1, help="Batch size, default: 1")
+    parser.add_argument('--resize', type=int, default=256, help="Resize images to this size, default: 256x256")
+    parser.add_argument('--calculate_metrics', type=bool, default=False, help="Calculate PSNR, SSIM and UIQM on test images")
+    parser.add_argument('--label_images_path', type=str, default="./data/label/", help='Path of ground truth images (clear images)')
 
     print("-------------------testing---------------------")
     config = parser.parse_args()
@@ -118,15 +134,27 @@ if __name__ == '__main__':
 
     start_time = time.time()
     testing(config)
-    print("total testing time", time.time() - start_time) ## more images for PFS test
-    # ### 以下是做测试
+    print("Total testing time:", time.time() - start_time)
+
+    # Directories for images
     GEN_im_dir = "./data/output/"
     GTr_im_dir = "./data/GT/"
+    Raw_im_dir = config.raw_images_path
+
+    # Calculate UIQM for generated images
     gen_uqims = measure_UIQMs(GEN_im_dir)
     print("Generated UQIM >> Mean: {0} std: {1}".format(np.mean(gen_uqims), np.std(gen_uqims)))
-    ### compute SSIM and PSNR
+
+    # Calculate UIQM for raw images
+    raw_uqims = measure_UIQMs(Raw_im_dir)
+    print("Raw UQIM >> Mean: {0} std: {1}".format(np.mean(raw_uqims), np.std(raw_uqims)))
+
+    # Calculate SSIM and PSNR for generated images
     SSIM_measures, PSNR_measures = SSIMs_PSNRs(GTr_im_dir, GEN_im_dir)
-    print("SSIM >> Mean: {0} std: {1}".format(np.mean(SSIM_measures), np.std(SSIM_measures)))
-    print("PSNR >> Mean: {0} std: {1}".format(np.mean(PSNR_measures), np.std(PSNR_measures)))
+    print("Generated SSIM >> Mean: {0} std: {1}".format(np.mean(SSIM_measures), np.std(SSIM_measures)))
+    print("Generated PSNR >> Mean: {0} std: {1}".format(np.mean(PSNR_measures), np.std(PSNR_measures)))
 
-
+    # Calculate SSIM and PSNR for raw images
+    SSIM_measures_raw, PSNR_measures_raw = SSIMs_PSNRs_raw(GTr_im_dir, Raw_im_dir)
+    print("Raw SSIM >> Mean: {0} std: {1}".format(np.mean(SSIM_measures_raw), np.std(SSIM_measures_raw)))
+    print("Raw PSNR >> Mean: {0} std: {1}".format(np.mean(PSNR_measures_raw), np.std(PSNR_measures_raw)))
